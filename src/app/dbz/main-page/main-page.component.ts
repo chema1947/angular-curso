5a2debb7dbb53b8a6e101785101263b203eaf963
chema1947 <librepensador69@gmail.com>
2022-07-20 14:37:54 +0200

    dbz

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+
+})
+export class MainPageComponent  {
+
+  
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Tortuga',
+    poder: 15000
+  }
+
+
+  constructor(){
+    
+
+  }
+  
+
+
+
+
+
+}
